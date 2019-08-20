124e51455cf46c062ba331377c1985d8edd6830c
Jun Hamamura <hmmrjn@gmail.com>
2019-08-20 19:29:28 +0900

    update to show todoList items

--- my-todo/TodoListViewController.swift
+++ my-todo/TodoListViewController.swift
@@ -12,24 +12,27 @@ class TodoListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var todoList: [Todo] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "todoCell")
         tableView.dataSource = self
+        todoList.append(Todo(title: "洗濯"))
     }
 
 }
 
 extension TodoListViewController: UITableViewDataSource {
     func  tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return todoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
         cell.accessoryType = .disclosureIndicator
-        cell.textLabel?.text = "\(indexPath.row)"
+        cell.textLabel?.text = todoList[indexPath.row].title
         return cell
     }
 }
---
+++ my-todo/todo.swift
@@ -0,0 +1,13 @@
+//
+//  todo.swift
+//  my-todo
+//
+//  Created by 浜村純 on 2019/08/20.
+//  Copyright © 2019 Jun Hamamura. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    let title: String
+}
