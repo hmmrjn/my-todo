447629d66bbb685fd9c538cc8c050ddac5b2e89e
Jun Hamamura <hmmrjn@gmail.com>
2019-08-20 19:20:39 +0900

    add list with 10 cells

--- my-todo/TodoListViewController.swift
+++ my-todo/TodoListViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class TodoListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "todoCell")
+        tableView.dataSource = self
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension TodoListViewController: UITableViewDataSource {
+    func  tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
 }
